a62e59f7aaf6c2f7ecff42e844b93bca146786a6
Default One <setdefaultone@gmail.com>
2024-07-26 10:47:13 +0600

    (frontend): Integrate SDK with backend.
    ## Changes
    
    - Add SDK for users collection.
    
    Signed-off-by: Default One <setdefaultone@gmail.com>

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,23 +3,63 @@ import { isQueryClientFail, isQuerySuccess, Sdk } from "@bootstrap-brand/sdk";
 export default async function Home() {
     const sdk = new Sdk({
         url: "http://localhost:4200/",
+        fetchOverrides: { cache: "no-cache" },
     });
 
-    const result = await sdk.ping.postPing({ message: "Ping!" });
+    let messageA = "";
+    try {
+        const result = await sdk.ping.postPing({ message: "Ping!" });
 
-    let message = "";
-    if (isQuerySuccess(result)) {
-        message = JSON.stringify(result.data, null, 4);
-    } else if (isQueryClientFail(result)) {
-        message = JSON.stringify(result.error);
-    } else {
-        message = result.message;
+        if (isQuerySuccess(result)) {
+            messageA = JSON.stringify(result.data, null, 4);
+        } else if (isQueryClientFail(result)) {
+            messageA = JSON.stringify(result.error);
+        } else {
+            messageA = result.message;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    let messageB = "";
+    try {
+        const result = await sdk.users.getUsers({ username: "default_one" });
+
+        if (isQuerySuccess(result)) {
+            messageB = JSON.stringify(result.data, null, 4);
+        } else if (isQueryClientFail(result)) {
+            messageB = JSON.stringify(result.error);
+        } else {
+            messageB = result.message;
+        }
+    } catch (e) {
+        console.error(e);
+    }
+
+    let messageC = "";
+    try {
+        const result = await sdk.users.getUser("6df19034-8089-445d-8935-7941c658d0e3");
+
+        if (isQuerySuccess(result)) {
+            messageC = JSON.stringify(result.data, null, 4);
+        } else if (isQueryClientFail(result)) {
+            messageC = JSON.stringify(result.error);
+        } else {
+            messageC = result.message;
+        }
+    } catch (e) {
+        console.error(e);
     }
 
     return (
         <main>
-            <h1>Bootstrap Brand</h1>
-            <pre>{message}</pre>
+            <h1 className="font-bold text-3xl">Bootstrap Brand</h1>
+            <hr />
+            <pre>{messageA}</pre>
+            <hr />
+            <pre>{messageB}</pre>
+            <hr />
+            <pre>{messageC}</pre>
         </main>
     );
 }
