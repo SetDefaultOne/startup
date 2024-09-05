d56faf8cdc01c21c276b660cef0d80d4d5de611f
Default One <setdefaultone@gmail.com>
2024-09-05 08:25:12 +0600

    fix(global): update package lock file versions

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,7 +9,7 @@ module.exports = {
         "header-trim": [2, "always"],
         "header-case": [2, "always", ["lower-case"]],
         "type-empty": [2, "never"],
-        "type-enum": [2, "always", ["feat"]],
+        "type-enum": [2, "always", ["feat", "fix"]],
         "scope-empty": [2, "never"],
         "scope-enum": [2, "always", ["global"]],
         "subject-empty": [2, "never"],
